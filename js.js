24a29215712ae7e5dc17fa1411dd4fd2acc067f9
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-04-14 15:10:48 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
-var box = document.getElementById("#caixinha");
-function clicar(){
-    document.getElementById("certo").style.color = "green";
-    document.getElementById("caixinha").value= document.getElementById("certo").innerText;
+function clicar(x,y){
+    document.getElementById(x).style.color = "green";
+    document.getElementById(y).value= document.getElementById(x).innerText;
 }
 function errado(){
     document.getElementsByClassName("r2").style.color = "red";

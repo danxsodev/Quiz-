cdfb926a291d3fb2498be5a7a2c1326eace7572b
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-04-14 15:18:24 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -2,6 +2,3 @@ function clicar(x,y){
     document.getElementById(x).style.color = "green";
     document.getElementById(y).value= document.getElementById(x).innerText;
 }
-function errado(){
-    document.getElementsByClassName("r2").style.color = "red";
-}
